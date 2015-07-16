c5690c953f9d100363922e61ccc2be460e2124bf
marinadecelis <marina88informatica@gmail.com>
2015-07-16 18:48:13 +0200

    nueva version en la rama master
    ejercicio mod7

--- models/models.js
+++ models/models.js
@@ -1,7 +1,37 @@
 var path = require('path');
 
+//hago esto para poder ejecutar npm start
+//cargo la variable auxiliar con variables de entorno si ejecuto foreman o la inicializo si ejecuto npm start, si ejecuto
+//npm start , DATABASE_URL es vacio
+var auxUrl = process.env.DATABASE_URL;
+var auxStorage = process.env.DATABASE_STORAGE;
+if(!auxUrl) auxUrl="sqlite://:@:/";
+if(!auxStorage) auxStorage="quiz.sqlite";
+
+var url = auxUrl.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name	= (url[6] || null);
+var user 	= (url[2]||null);
+var pwd 	= (url[3]||null);
+var protocol= (url[1]||null);
+var dialect	= (url[1]||null);
+var port	= (url[5]||null);
+var host	= (url[4]||null);
+var storage	= auxStorage;
+
 //Cargar modelo ORM
 var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(DB_name, user, pwd,
+	{	dialect:	protocol,
+		protocol:	protocol,
+		port:		port,
+		host:		host,
+		storage: 	storage,
+		omiNull:	true
+	}
+);
+
+
 
 //usar BBDD SQlite:
 var sequelize = new Sequelize(null,null,null,
